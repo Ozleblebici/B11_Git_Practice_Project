bd53101e5d4cff23367d34ff59ec8de3d3c4f139
zkartal <kartalzafer52@gmail.com>
2023-09-19 20:58:36 +0200

    P&R degisikleri uygulandi

--- src/ISIMLER/Zafer.java
+++ src/ISIMLER/Zafer.java
@@ -3,6 +3,6 @@ package ISIMLER;
 public class Zafer {
     public static void main(String[] args) {
         System.out.println("zafer local");
-        System.out.println("zafer");
+        System.out.println("zafer Kartal");
     }
 }
