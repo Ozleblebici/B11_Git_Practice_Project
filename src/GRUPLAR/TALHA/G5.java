d16339c81d5f4afd3cca32cd74bda73788c7685f
tozleblebici <talha43gs@gmail.com>
2023-09-20 17:24:09 +0100

    Common methods updated

--- src/GRUPLAR/TALHA/G5.java
+++ src/GRUPLAR/TALHA/G5.java
@@ -5,4 +5,19 @@ public class G5 {
         System.out.println("SEYIT");
         System.out.println("TALHA");
     }
+    public void verifyOrder(){
+        System.out.println("order verified");
+    }
+
+    public void receiveOrderInfo(){
+        System.out.println("order info received");
+    }
+
+    public void pauseCode(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
