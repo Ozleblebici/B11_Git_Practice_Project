7ab805dbfb62d7d829b4fdd2f154a7d8d05af388
Seyit <seyitaligermany@gmail.com>
2023-09-20 14:34:09 +0200

    G2.java dosyasi guncellendi

--- src/GRUPLAR/G2/G2.java
+++ src/GRUPLAR/G2/G2.java
@@ -2,7 +2,8 @@ package GRUPLAR.G2;
 
 public class G2 {
     public static void main(String[] args) {
-        System.out.println("Seyit alinin locali");
+        System.out.println("Seyit ali icin conflict");
         System.out.println("Bilal`in locali");
+        System.out.println("Seyit ali'nin locali");
     }
 }
