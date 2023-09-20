fac6d462f44618a1dfe33972ab0db3bcda388dad
Seyit Ali <seyitaligermany@gmail.com>
2023-09-20 14:17:16 +0200

    Update Seyit2.java

--- src/GRUPLAR/G2/Seyit2.java
+++ src/GRUPLAR/G2/Seyit2.java
@@ -3,5 +3,6 @@ package GRUPLAR.G2;
 public class Seyit2 {
     public static void main(String[] args) {
         System.out.println("Seyit Ali Oran");
+         System.out.println("Almanya");
     }
 }
