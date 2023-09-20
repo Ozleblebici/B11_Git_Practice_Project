f5d94ff17f337f3c83f90987054fd09cb0412bff
bilaloender <bilaloender@yahoo.com>
2023-09-20 14:26:05 +0200

    Yasadigim ülke eklendi.

--- src/GRUPLAR/G2/bilal2.java
+++ src/GRUPLAR/G2/bilal2.java
@@ -3,5 +3,7 @@ package GRUPLAR.G2;
 public class bilal2 {
     public static void main(String[] args) {
         System.out.println("Bilal Oender 2. project");
+        System.out.println("Almanya");
+
     }
 }
