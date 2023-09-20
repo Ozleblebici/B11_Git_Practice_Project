d5951394bdfe3a7c51732d111c24f6aa08f8112c
bilaloender <bilaloender@yahoo.com>
2023-09-20 14:34:06 +0200

    G2 dosyasi degistirildi

--- src/GRUPLAR/G2/G2.java
+++ src/GRUPLAR/G2/G2.java
@@ -4,5 +4,6 @@ public class G2 {
     public static void main(String[] args) {
         System.out.println("Seyit alinin locali");
         System.out.println("Bilal`in locali");
+        System.out.println("Bilal icin confict");
     }
 }
