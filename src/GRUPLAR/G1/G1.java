09f3ad2fdd8c6d9d90514f78249ef81491a7e75c
zkartal <kartalzafer52@gmail.com>
2023-09-21 14:41:17 +0200

    Merge Konusu güncellendi

--- src/GRUPLAR/G1/G1.java
+++ src/GRUPLAR/G1/G1.java
@@ -6,5 +6,9 @@ public class G1 {
         System.out.println("Zafer");
         System.out.println("Furkan ve Zafer");
         System.out.println("Zafer ve Furkan");
+
+        System.out.println("Merge Konusu");
+        System.out.println("Merge Konusu");
+        System.out.println("Merge Konmusu");
     }
 }
